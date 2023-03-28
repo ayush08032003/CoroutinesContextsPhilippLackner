343314cd08097296b021f415937cb7ea954c50c7
Ayush Kumar <ayushkumar08032003@gmail.com>
2023-03-28 11:24:25 +0530

    Add Some Notes and Do some changes in Delay Timing..

--- app/src/main/java/com/example/coroutinescontexts/MainActivity.kt
+++ app/src/main/java/com/example/coroutinescontexts/MainActivity.kt
@@ -25,7 +25,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     suspend fun doNetworkCall():String{
-        delay(5000L)
+        delay(3000L)
         return "This is the Answer"
     }
 }
+
+/*
+NOTES/STEPS:-
+In General Coroutines is always stared in a particular context. For now we only use GlobalScope.launch{} to start a coroutine, but that didn't give us much
+control over it.
+But we can get Dispatchers as launch functions
+Dispatcher.Main -> A coroutine dispatcher that is confined to the Main thread operating with UI objects. It will start a coroutine in the Main Thread.
+Dispatcher.IO -> Useful for all Kind of data operations such as Networking, Data Operations, Databases and Reading and writing a File.
+Dispatcher.Default -> If you are planning on Doing complex operations and long-running calculations that will block the main thread
+Dispatcher.Unconfined ->
+
+Can ALso use our own new Thread by using newSingleThreadContext("MyThread")
+ALso can Switch the Context with the Main Dispatcher using withContext(Dispatcher.Main) function.
+
+ */
